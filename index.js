e8f512aafc3abe6a4ff19ddaedee16029a4a0784
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-10-09 17:21:25 -0300

    chore: 🚧 Work in progress

--- index.js
+++ index.js
@@ -1,41 +1,58 @@
-import { select, input, checkbox } from '@inquirer/prompts';
+import { select, input, checkbox } from "@inquirer/prompts";
+import { promises as fs } from "fs";
 
-const goal1 = {
-  value: 'Ser um programador fullstack JS',
-  checked: true,
+let message = "Bem vindos ao App de Metas!";
+let goals = [];
+
+const loadingGoals = async () => {
+  try {
+    const data = await fs.readFile("goals.json", "utf8");
+    goals = JSON.parse(data);
+  } catch (error) {
+    goals = [];
+  }
 };
 
-let goals = [];
+const saveGoals = async () => {
+  await fs.writeFile("goals.json", JSON.stringify(goals, null, 2));
+};
 
 const registerGoal = async () => {
   const goal = await input({
-    message: 'Digite a meta:',
+    message: "Digite a meta:",
   });
 
   if (goal.length === 0) {
-    console.log('A meta não pode ser vazia!');
+    message = "A meta não pode ser vazia!";
     return;
   }
 
   goals.push({ value: goal, checked: false });
+
+  message = "Meta cadastrada com sucesso!";
 };
 
 const showGoals = async () => {
+  if (goals.length === 0) {
+    message = "Não existem metas!";
+    return;
+  }
+
   const responses = await checkbox({
     message:
-      'Use as SETAS para navegar, o ESPAÇO para marcar/desmarcar e o ENTER para finalizar:',
+      "Use as SETAS para navegar, o ESPAÇO para marcar/desmarcar e o ENTER para finalizar:",
     choices: [...goals],
     instructions: false,
   });
 
-  if (responses.length === 0) {
-    console.log('Nenhuma meta foi selecionada!');
-    return;
-  }
-
   goals.forEach((goal) => {
     goal.checked = false;
   });
+
+  if (responses.length === 0) {
+    message = "Nenhuma meta foi selecionada!";
+    return;
+  }
 
   responses.forEach((response) => {
     const goal = goals.find((g) => {
@@ -45,33 +62,127 @@ const showGoals = async () => {
     goal.checked = true;
   });
 
-  console.log('Meta(s) marcada(a) como concluída(s)');
+  message = "Meta(s) marcada(as) como concluída(s)";
+};
+
+const goalsAchieved = async () => {
+  if (goals.length === 0) {
+    message = "Não existem metas!";
+    return;
+  }
+
+  const achievers = goals.filter((goal) => {
+    return goal.checked;
+  });
+
+  if (achievers.length === 0) {
+    message = "Nenhuma meta foi realizada! :(";
+    return;
+  }
+
+  await select({
+    message: "Metas realizadas: " + achievers.length,
+    choices: [...achievers],
+  });
+};
+
+const openGoals = async () => {
+  if (goals.length === 0) {
+    message = "Não existem metas!";
+    return;
+  }
+
+  const open = goals.filter((goal) => {
+    return !goal.checked;
+  });
+
+  if (open.length === 0) {
+    message = "Nenhuma meta está aberta!";
+    return;
+  }
+
+  await select({
+    message: "Metas abertas: " + open.length,
+    choices: [...open],
+  });
+};
+
+const deleteGoals = async () => {
+  if (goals.length === 0) {
+    message = "Não existem metas!";
+    return;
+  }
+
+  const unmarkedGoals = goals.map((goal) => {
+    return { value: goal.value, checked: false };
+  });
+
+  const itemsToDelete = await checkbox({
+    message: "Selecione uma meta para deletar:",
+    choices: [...unmarkedGoals],
+    instructions: false,
+  });
+
+  if (itemsToDelete.length === 0) {
+    message = "Nenhuma meta para deletar!";
+    return;
+  }
+
+  itemsToDelete.forEach((item) => {
+    goals = goals.filter((goal) => {
+      return goal.value !== item;
+    });
+  });
+  message = "Meta(s) deletada(s) com sucesso!";
+};
+
+const showMessages = () => {
+  console.clear();
+
+  if (message !== "") {
+    console.log(message);
+    console.log("");
+    message = "";
+  }
 };
 
 const start = async () => {
+  await loadingGoals();
+
   while (true) {
+    showMessages();
+    await saveGoals();
+
     const option = await select({
-      message: 'Menu >',
+      message: "Menu >",
       choices: [
-        { name: 'Cadastrar Metas', value: 'cadastrar' },
-        { name: 'Listar Metas', value: 'listar' },
-        { name: 'Buscar Metas', value: 'buscar' },
-        { name: 'Sair', value: 'sair' },
+        { name: "Cadastrar Metas", value: "cadastrar" },
+        { name: "Listar Metas", value: "listar" },
+        { name: "Metas realizadas", value: "realizadas" },
+        { name: "Metas abertas", value: "abertas" },
+        { name: "Deletar metas", value: "deletar" },
+        { name: "Sair", value: "sair" },
       ],
     });
 
     switch (option) {
-      case 'cadastrar':
+      case "cadastrar":
         await registerGoal();
         break;
-      case 'listar':
+      case "listar":
         await showGoals();
         break;
-      case 'buscar':
-        console.log('Buscou com sucesso!');
+      case "realizadas":
+        await goalsAchieved();
         break;
-      case 'sair':
-        console.log('Até a próxima!');
+      case "abertas":
+        await openGoals();
+        break;
+      case "deletar":
+        await deleteGoals();
+        break;
+      case "sair":
+        console.log("Até a próxima!");
         return;
     }
   }
